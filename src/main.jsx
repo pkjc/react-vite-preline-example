f40fbc136aaaaec26d854dabe48be0400f61bbe8
ashikck <84653160+ashikck@users.noreply.github.com>
2023-12-05 11:43:08 +0530

    Preline component 2.0.2

--- src/main.jsx
+++ src/main.jsx
@@ -6,15 +6,15 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import App from "./App";
+import Preline from "./components/Preline";
 import CalenderPage from "./pages/CalenderPage";
 import DashboardPage from "./pages/DashboardPage";
 import DocumentationPage from "./pages/DocumentationPage";
 
-import("preline");
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
+      <Preline />
       <Routes>
         <Route path='/' element={<App />}>
           <Route path='/home' element={<DashboardPage />} />
